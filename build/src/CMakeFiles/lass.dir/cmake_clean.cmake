44e6abfd3436bfa35d8f1a2472136a9731ab8cfc
Manuel Baptista <mbaptist@gmail.com>
2015-05-05 02:31:56 +0200

    Added cmake build system

---
+++ build/src/CMakeFiles/lass.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lass.dir/cgsolver/cgsolver.C.o"
+  "liblass.pdb"
+  "liblass.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/lass.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
